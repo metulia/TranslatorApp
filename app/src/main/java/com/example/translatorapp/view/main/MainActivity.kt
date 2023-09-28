965d9c3d9a83f96b1b76898520b03235700eb840
Yuliia Stepkina <metulia@mail.ru>
2023-09-28 12:45:56 +0300

    Урок 8. Добавила Splash Screen, добавила RenderEffect при подгрузке картинки в DescriptionActivity.kt

--- app/src/main/java/com/example/translatorapp/view/main/MainActivity.kt
+++ app/src/main/java/com/example/translatorapp/view/main/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.translatorapp.view.main
 
+import android.animation.ObjectAnimator
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.view.ViewTreeObserver
+import android.view.animation.AnticipateInterpolator
+import androidx.annotation.RequiresApi
+import androidx.core.animation.doOnEnd
 import androidx.lifecycle.Observer
 import com.example.core.BaseActivity
 import com.example.model.AppState
@@ -18,6 +25,10 @@ import com.example.translatorapp.view.history.HistoryActivity
 import com.example.translatorapp.viewmodel.main.MainInteractor
 import com.example.translatorapp.viewmodel.main.MainViewModel
 import org.koin.android.ext.android.inject
+
+private const val SLIDE_LEFT_DURATION = 1000L
+private const val COUNTDOWN_DURATION = 2000L
+private const val COUNTDOWN_INTERVAL = 1000L
 
 class MainActivity : BaseActivity<AppState, MainInteractor>() {
 
@@ -38,9 +49,9 @@ class MainActivity : BaseActivity<AppState, MainInteractor>() {
                 startActivity(
                     DescriptionActivity.getIntent(
                         this@MainActivity,
-                        data.text!!,
-                        convertMeaningsToSingleString(data.meanings!!),
-                        data.meanings!![0].imageUrl
+                        data.text,
+                        convertMeaningsToSingleString(data.meanings),
+                        data.meanings[0].imageUrl
                     )
                 )
             }
@@ -65,6 +76,8 @@ class MainActivity : BaseActivity<AppState, MainInteractor>() {
 
         iniViewModel()
         initViews()
+
+        setDefaultSplashScreen()
     }
 
     override fun setDataToAdapter(data: List<DataModel>) {
@@ -100,5 +113,58 @@ class MainActivity : BaseActivity<AppState, MainInteractor>() {
     private fun initViews() {
         binding.searchFab.setOnClickListener(fabClickListener)
         binding.mainActivityRecyclerview.adapter = adapter
+    }
+
+    private fun setDefaultSplashScreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            setSplashScreenHideAnimation()
+            setSplashScreenDuration()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun setSplashScreenHideAnimation() {
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            val slideLeft = ObjectAnimator.ofFloat(
+                splashScreenView,
+                View.TRANSLATION_X,
+                0f,
+                -splashScreenView.height.toFloat()
+            )
+
+            slideLeft.interpolator = AnticipateInterpolator()
+            slideLeft.duration = SLIDE_LEFT_DURATION
+
+            slideLeft.doOnEnd { splashScreenView.remove() }
+            slideLeft.start()
+        }
+    }
+
+    private fun setSplashScreenDuration() {
+        var isHideSplashScreen = false
+
+        object : CountDownTimer(COUNTDOWN_DURATION, COUNTDOWN_INTERVAL) {
+            override fun onTick(p0: Long) {
+                // Nothing to do
+            }
+
+            override fun onFinish() {
+                isHideSplashScreen = true
+            }
+        }.start()
+
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    return if (isHideSplashScreen) {
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        false
+                    }
+                }
+            }
+        )
     }
 }
